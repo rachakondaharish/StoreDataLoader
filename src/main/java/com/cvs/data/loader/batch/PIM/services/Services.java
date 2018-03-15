6e1d0e97f2cfcf3fe15b313637ab771afa64969d
Rachakonda <hrachako@LAMU02SGE4KG8WP.uhc.com>
2018-03-15 02:48:16 -0500

    added if conditions

--- src/main/java/com/cvs/data/loader/batch/PIM/services/Services.java
+++ src/main/java/com/cvs/data/loader/batch/PIM/services/Services.java
@@ -43,31 +43,56 @@ public class Services {
 					Element eElement = (Element) nNode;
 					StoreTempEntity store = new StoreTempEntity();
 					store.setStoreId(Integer.parseInt(eElement.getAttribute("Id")));
+					
 					store.setSTR_NBR(Integer.parseInt(eElement.getElementsByTagName("tns:STR_NBR").item(0).getTextContent()));
-					store.setSTAT_CD(eElement.getElementsByTagName("tns:STAT_CD").item(0).getTextContent());
-					store.setSTREET_ADDR1(eElement.getElementsByTagName("tns:STREET_ADDR1").item(0).getTextContent());
-					store.setCITY_NAME(eElement.getElementsByTagName("tns:CITY_NAME").item(0).getTextContent());
-					store.setSTATE_CD(eElement.getElementsByTagName("tns:STATE_CD").item(0).getTextContent());
-					store.setZIP_CD(eElement.getElementsByTagName("tns:ZIP_CD").item(0).getTextContent());
-					store.setCOUNTY_CD(eElement.getElementsByTagName("tns:COUNTY_CD").item(0).getTextContent());
-					store.setCOUNTY_CD_DSC(eElement.getElementsByTagName("tns:COUNTY_CD_DSC").item(0).getTextContent());
-					store.setOPCO_DSC(eElement.getElementsByTagName("tns:OPCO_DSC").item(0).getTextContent());
-					store.setBUS_SEGMENT(eElement.getElementsByTagName("tns:BUS_SEGMENT").item(0).getTextContent());
-					store.setFACILITY_TYP_CD(eElement.getElementsByTagName("tns:FACILITY_TYP_CD").item(0).getTextContent());
-					store.setFACILITY_TYP_DSC(eElement.getElementsByTagName("tns:FACILITY_TYP_DSC").item(0).getTextContent());
-					store.setDIV_NAME(eElement.getElementsByTagName("tns:DIV_NAME").item(0).getTextContent());
-					store.setFS_AREA_NBR(eElement.getElementsByTagName("tns:FS_AREA_NBR").item(0).getTextContent());
-					store.setFS_REGION_NBR(eElement.getElementsByTagName("tns:FS_REGION_NBR").item(0).getTextContent());
-					store.setFS_DISTRICT_NBR(eElement.getElementsByTagName("tns:FS_DISTRICT_NBR").item(0).getTextContent());
-					store.setRX_AREA_NBR(eElement.getElementsByTagName("tns:RX_AREA_NBR").item(0).getTextContent());
-					store.setRX_REGION_NBR(eElement.getElementsByTagName("tns:RX_REGION_NBR").item(0).getTextContent());
-					store.setRX_DISTRICT_NBR(eElement.getElementsByTagName("tns:RX_DISTRICT_NBR").item(0).getTextContent());
-					store.setLATITUDE(eElement.getElementsByTagName("tns:LATITUDE").item(0).getTextContent());
-					store.setLONGITUDE(eElement.getElementsByTagName("tns:LONGITUDE").item(0).getTextContent());
-					store.setRX_PHONE_NBR(eElement.getElementsByTagName("tns:RX_PHONE_NBR").item(0).getTextContent());
-					store.setDSTR_MGR_FNME(eElement.getElementsByTagName("tns:DSTR_MGR_FNME").item(0).getTextContent());
-					store.setDSTR_MGR_LNME(eElement.getElementsByTagName("tns:DSTR_MGR_LNME").item(0).getTextContent());
-					store.setDSTR_MGR_EMAIL(eElement.getElementsByTagName("tns:DSTR_MGR_EMAIL").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:STAT_CD").getLength()>0)
+						store.setSTAT_CD(eElement.getElementsByTagName("tns:STAT_CD").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:STREET_ADDR1").getLength()>0)
+						store.setSTREET_ADDR1(eElement.getElementsByTagName("tns:STREET_ADDR1").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:CITY_NAME").getLength()>0)
+						store.setCITY_NAME(eElement.getElementsByTagName("tns:CITY_NAME").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:STATE_CD").getLength()>0)
+						store.setSTATE_CD(eElement.getElementsByTagName("tns:STATE_CD").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:ZIP_CD").getLength()>0)
+						store.setZIP_CD(eElement.getElementsByTagName("tns:ZIP_CD").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:COUNTY_CD").getLength()>0)
+						store.setCOUNTY_CD(eElement.getElementsByTagName("tns:COUNTY_CD").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:COUNTY_CD_DSC").getLength()>0)
+						store.setCOUNTY_CD_DSC(eElement.getElementsByTagName("tns:COUNTY_CD_DSC").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:OPCO_DSC").getLength()>0)
+						store.setOPCO_DSC(eElement.getElementsByTagName("tns:OPCO_DSC").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:BUS_SEGMENT").getLength()>0)
+						store.setBUS_SEGMENT(eElement.getElementsByTagName("tns:BUS_SEGMENT").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:FACILITY_TYP_CD").getLength()>0)
+						store.setFACILITY_TYP_CD(eElement.getElementsByTagName("tns:FACILITY_TYP_CD").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:FACILITY_TYP_DSC").getLength()>0)
+						store.setFACILITY_TYP_DSC(eElement.getElementsByTagName("tns:FACILITY_TYP_DSC").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:DIV_NAME").getLength()>0)
+						store.setDIV_NAME(eElement.getElementsByTagName("tns:DIV_NAME").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:FS_AREA_NBR").getLength()>0)
+						store.setFS_AREA_NBR(eElement.getElementsByTagName("tns:FS_AREA_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:FS_REGION_NBR").getLength()>0)
+						store.setFS_REGION_NBR(eElement.getElementsByTagName("tns:FS_REGION_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:FS_DISTRICT_NBR").getLength()>0)
+						store.setFS_DISTRICT_NBR(eElement.getElementsByTagName("tns:FS_DISTRICT_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:RX_AREA_NBR").getLength()>0)
+						store.setRX_AREA_NBR(eElement.getElementsByTagName("tns:RX_AREA_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:RX_REGION_NBR").getLength()>0)
+						store.setRX_REGION_NBR(eElement.getElementsByTagName("tns:RX_REGION_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:RX_DISTRICT_NBR").getLength()>0)
+						store.setRX_DISTRICT_NBR(eElement.getElementsByTagName("tns:RX_DISTRICT_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:LATITUDE").getLength()>0)
+						store.setLATITUDE(eElement.getElementsByTagName("tns:LATITUDE").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:LONGITUDE").getLength()>0)
+						store.setLONGITUDE(eElement.getElementsByTagName("tns:LONGITUDE").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:RX_PHONE_NBR").getLength()>0)
+						store.setRX_PHONE_NBR(eElement.getElementsByTagName("tns:RX_PHONE_NBR").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:DSTR_MGR_FNME").getLength()>0)
+						store.setDSTR_MGR_FNME(eElement.getElementsByTagName("tns:DSTR_MGR_FNME").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:DSTR_MGR_LNME").getLength()>0)
+						store.setDSTR_MGR_LNME(eElement.getElementsByTagName("tns:DSTR_MGR_LNME").item(0).getTextContent());
+					if(eElement.getElementsByTagName("tns:DSTR_MGR_EMAIL").getLength()>0)
+						store.setDSTR_MGR_EMAIL(eElement.getElementsByTagName("tns:DSTR_MGR_EMAIL").item(0).getTextContent());
 					
 					storeRepository.saveAndFlush(store);
 				}
