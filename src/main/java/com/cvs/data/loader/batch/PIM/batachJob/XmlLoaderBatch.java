712ab19a87450d7478f81caa260d92c4a9675ae1
Rachakonda <hrachako@LAMU02SGE4KG8WP.uhc.com>
2018-03-16 07:26:43 -0500

    added jaxb

--- src/main/java/com/cvs/data/loader/batch/PIM/batachJob/XmlLoaderBatch.java
+++ src/main/java/com/cvs/data/loader/batch/PIM/batachJob/XmlLoaderBatch.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Component;
 
 import com.cvs.data.loader.batch.PIM.constants.Constants;
 import com.cvs.data.loader.batch.PIM.services.Services;
+import com.cvs.data.loader.batch.parseXml.STORES;
 
 @Component
 public class XmlLoaderBatch {
@@ -19,32 +24,45 @@ public class XmlLoaderBatch {
 	Services services;
 	@Autowired
 	Constants constants;
-	
-	@Scheduled(cron = "${store.data.load.scheduler}")
-	//@PostConstruct
+
+	 @Scheduled(cron = "${store.data.load.scheduler}")
+	// @PostConstruct
 	public void runBatch() {
 		System.out.println("Looking for files: " + new Date());
 		try {
 			File sourceXml = new File(constants.sourcepath);
 			File destinationXML = new File(constants.destinationpath);
 			File resourcesXML = new File(constants.resourcespath);
-			
+
 			if (sourceXml.exists() && !sourceXml.isDirectory()) {
 				System.out.println("File found.\nCopying to resources for processing: " + sourceXml);
 				services.copyfile(sourceXml, resourcesXML);
-				
-				services.xmlToDatabase(resourcesXML);//Read and store to database
-				
+
+				services.xmlToDatabaseUsingJAXB(resourcesXML);// Read and store to
+														// database
+
 				services.moveFromTempToStores();
-				
+
 				services.copyfile(resourcesXML, destinationXML);
-				
+
 				services.cleanup(sourceXml, resourcesXML);
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	//@Scheduled(cron = "${store.data.load.scheduler}")
+	public static void marshalXml() throws Exception {
+		JAXBContext jc = JAXBContext.newInstance(STORES.class);
+		
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+		STORES stores = (STORES) unmarshaller.unmarshal(new File("/Users/hrachako/Documents/Personal/files/cvs.xml"));
+
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		
 	}
 
 }
--- src/main/java/com/cvs/data/loader/batch/PIM/services/Services.java
+++ src/main/java/com/cvs/data/loader/batch/PIM/services/Services.java
@@ -4,9 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.catalina.Store;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,7 +24,8 @@ import com.cvs.data.loader.batch.PIM.entities.StoreTempEntity;
 import com.cvs.data.loader.batch.PIM.entities.StoresEntity;
 import com.cvs.data.loader.batch.PIM.jpa.StoreTempRepository;
 import com.cvs.data.loader.batch.PIM.jpa.StoresRepository;
-
+import com.cvs.data.loader.batch.parseXml.STORE;
+import com.cvs.data.loader.batch.parseXml.STORES;
 
 @Component
 public class Services {
@@ -27,91 +33,70 @@ public class Services {
 	StoreTempRepository storeRepository;
 	@Autowired
 	StoresRepository storesRepository;
+
 	
-	public void xmlToDatabase(File fXmlFile){
+	public void xmlToDatabaseUsingJAXB(File fXmlFile) {
+		JAXBContext jc;
 		try {
-	    	System.out.println("Storing XML: "+fXmlFile.getPath()+" to database.");
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
-			doc.getDocumentElement().normalize();
-			NodeList nList = doc.getElementsByTagName("tns:STORE");
-			System.out.println("Store records found: "+nList.getLength());
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					StoreTempEntity store = new StoreTempEntity();
-					store.setStoreId(Integer.parseInt(eElement.getAttribute("Id")));
-					
-					store.setSTR_NBR(Integer.parseInt(eElement.getElementsByTagName("tns:STR_NBR").item(0).getTextContent()));
-					if(eElement.getElementsByTagName("tns:STAT_CD").getLength()>0)
-						store.setSTAT_CD(eElement.getElementsByTagName("tns:STAT_CD").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:STREET_ADDR1").getLength()>0)
-						store.setSTREET_ADDR1(eElement.getElementsByTagName("tns:STREET_ADDR1").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:CITY_NAME").getLength()>0)
-						store.setCITY_NAME(eElement.getElementsByTagName("tns:CITY_NAME").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:STATE_CD").getLength()>0)
-						store.setSTATE_CD(eElement.getElementsByTagName("tns:STATE_CD").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:ZIP_CD").getLength()>0)
-						store.setZIP_CD(eElement.getElementsByTagName("tns:ZIP_CD").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:COUNTY_CD").getLength()>0)
-						store.setCOUNTY_CD(eElement.getElementsByTagName("tns:COUNTY_CD").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:COUNTY_CD_DSC").getLength()>0)
-						store.setCOUNTY_CD_DSC(eElement.getElementsByTagName("tns:COUNTY_CD_DSC").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:OPCO_DSC").getLength()>0)
-						store.setOPCO_DSC(eElement.getElementsByTagName("tns:OPCO_DSC").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:BUS_SEGMENT").getLength()>0)
-						store.setBUS_SEGMENT(eElement.getElementsByTagName("tns:BUS_SEGMENT").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:FACILITY_TYP_CD").getLength()>0)
-						store.setFACILITY_TYP_CD(eElement.getElementsByTagName("tns:FACILITY_TYP_CD").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:FACILITY_TYP_DSC").getLength()>0)
-						store.setFACILITY_TYP_DSC(eElement.getElementsByTagName("tns:FACILITY_TYP_DSC").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:DIV_NAME").getLength()>0)
-						store.setDIV_NAME(eElement.getElementsByTagName("tns:DIV_NAME").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:FS_AREA_NBR").getLength()>0)
-						store.setFS_AREA_NBR(eElement.getElementsByTagName("tns:FS_AREA_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:FS_REGION_NBR").getLength()>0)
-						store.setFS_REGION_NBR(eElement.getElementsByTagName("tns:FS_REGION_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:FS_DISTRICT_NBR").getLength()>0)
-						store.setFS_DISTRICT_NBR(eElement.getElementsByTagName("tns:FS_DISTRICT_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:RX_AREA_NBR").getLength()>0)
-						store.setRX_AREA_NBR(eElement.getElementsByTagName("tns:RX_AREA_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:RX_REGION_NBR").getLength()>0)
-						store.setRX_REGION_NBR(eElement.getElementsByTagName("tns:RX_REGION_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:RX_DISTRICT_NBR").getLength()>0)
-						store.setRX_DISTRICT_NBR(eElement.getElementsByTagName("tns:RX_DISTRICT_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:LATITUDE").getLength()>0)
-						store.setLATITUDE(eElement.getElementsByTagName("tns:LATITUDE").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:LONGITUDE").getLength()>0)
-						store.setLONGITUDE(eElement.getElementsByTagName("tns:LONGITUDE").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:RX_PHONE_NBR").getLength()>0)
-						store.setRX_PHONE_NBR(eElement.getElementsByTagName("tns:RX_PHONE_NBR").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:DSTR_MGR_FNME").getLength()>0)
-						store.setDSTR_MGR_FNME(eElement.getElementsByTagName("tns:DSTR_MGR_FNME").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:DSTR_MGR_LNME").getLength()>0)
-						store.setDSTR_MGR_LNME(eElement.getElementsByTagName("tns:DSTR_MGR_LNME").item(0).getTextContent());
-					if(eElement.getElementsByTagName("tns:DSTR_MGR_EMAIL").getLength()>0)
-						store.setDSTR_MGR_EMAIL(eElement.getElementsByTagName("tns:DSTR_MGR_EMAIL").item(0).getTextContent());
-					
-					storeRepository.saveAndFlush(store);
+			jc = JAXBContext.newInstance(STORES.class);
+
+			Unmarshaller unmarshaller = jc.createUnmarshaller();
+			STORES stores = (STORES) unmarshaller
+					.unmarshal(fXmlFile);
+
+			Marshaller marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			for(int i=0; i<stores.getSTORE().size(); i++ ){
+				STORE xmlStore = stores.getSTORE().get(i);
+				StoreTempEntity store = new StoreTempEntity();
+
+				store.setStoreId(xmlStore.getSTATCD());
+				store.setSTR_NBR(Integer.parseInt(xmlStore.getSTRNBR()));
+				store.setSTAT_CD(xmlStore.getSTATCD().toString());
+				store.setSTREET_ADDR1(xmlStore.getSTREETADDR1());
+				store.setCITY_NAME(xmlStore.getCITYNAME());
+				store.setSTATE_CD(xmlStore.getSTATECD());
+				store.setZIP_CD(xmlStore.getZIPCD());
+				store.setCOUNTY_CD(xmlStore.getCOUNTYCD());
+				store.setCOUNTY_CD_DSC(xmlStore.getCOUNTYCDDSC());
+				store.setOPCO_DSC(xmlStore.getOPCODSC());
+				store.setBUS_SEGMENT(xmlStore.getBUSSEGMENT());
+				store.setFACILITY_TYP_CD(xmlStore.getFACILITYTYPCD());
+				store.setFACILITY_TYP_DSC(xmlStore.getFACILITYTYPDSC());
+				store.setDIV_NAME(xmlStore.getDIVNAME());
+				store.setFS_AREA_NBR(xmlStore.getFSAREANBR());
+				store.setFS_REGION_NBR(xmlStore.getFSREGIONNBR());
+				store.setFS_DISTRICT_NBR(xmlStore.getFSDISTRICTNBR());
+				store.setRX_AREA_NBR(xmlStore.getRXAREANBR());
+				store.setRX_REGION_NBR(xmlStore.getRXREGIONNBR());
+				store.setRX_DISTRICT_NBR(xmlStore.getRXDISTRICTNBR());
+				store.setLATITUDE(xmlStore.getLATITUDE());
+				store.setLONGITUDE(xmlStore.getLONGITUDE());
+				store.setRX_PHONE_NBR(xmlStore.getRXAREANBR());
+				store.setDSTR_MGR_FNME(xmlStore.getDSTRMGRFNME());
+				store.setDSTR_MGR_LNME(xmlStore.getDSTRMGRLNME());
+				store.setDSTR_MGR_EMAIL(xmlStore.getDSTRMGREMAIL());
+
+				storeRepository.saveAndFlush(store);
+				if (i % 10 == 0) {
+				     System.out.println("Inserted to temp table: "+i+" records.");
 				}
+				
 			}
-		} catch (Exception e) {
+			
+		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
-
 	}
-	
-	
-	public void moveFromTempToStores(){
+
+	public void moveFromTempToStores() {
 		List<StoreTempEntity> tempList = storeRepository.findAll();
-		System.out.println("Records list: "+tempList.size());
-		for(int i=0;i<tempList.size(); i++ ){
+		System.out.println("Records list: " + tempList.size());
+		for (int i = 0; i < tempList.size(); i++) {
 			StoreTempEntity storeTempEntity = tempList.get(i);
 			StoresEntity storesEntity = new StoresEntity();
 			storesEntity.setStoreId(storeTempEntity.getStoreId());
-			
+
 			storesEntity.setSTR_NBR(storeTempEntity.getSTR_NBR());
 			storesEntity.setSTAT_CD(storeTempEntity.getSTAT_CD());
 			storesEntity.setSTREET_ADDR1(storeTempEntity.getSTREET_ADDR1());
@@ -137,21 +122,22 @@ public class Services {
 			storesEntity.setDSTR_MGR_FNME(storeTempEntity.getDSTR_MGR_FNME());
 			storesEntity.setDSTR_MGR_LNME(storeTempEntity.getDSTR_MGR_LNME());
 			storesEntity.setDSTR_MGR_EMAIL(storeTempEntity.getDSTR_MGR_EMAIL());
-			
-			
-			try{
+
+			try {
 				storesRepository.saveAndFlush(storesEntity);
 				storeTempEntity.setDeleteStatus(true);
 				storeRepository.saveAndFlush(storeTempEntity);
-			}catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			
+			if (i % 10 == 0) {
+			     System.out.println("Records moved to final table: "+i);
+			}
 		}
-		
-		
+
 	}
-	
-	
+
 	public void cleanup(File file1, File file2) {
 		if (file1.delete() && file2.delete()) {
 			System.out.println("Source File and Resources temp files are deleted successfully");
